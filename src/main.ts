c7c289767465c495d30daa455ef319082036a137
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2025-11-20 09:34:51 +0800

    add cookie-parser and use of credential for cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { doubleCsrf } from 'csrf-csrf';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
